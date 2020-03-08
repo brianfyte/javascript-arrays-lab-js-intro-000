13e306491015b0ac08bbbe58b14bdd9d9b52fe4b
Brian Wong <brianw385@gmail.com>
2020-03-08 19:09:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function desctructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
